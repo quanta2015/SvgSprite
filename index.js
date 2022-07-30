e8ef362a0645e10af1123e06c62183fddd4086ae
quanta2015 <liyangtom@163.com>
2022-07-30 17:42:59 +0800

    update

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
+// import sprite from './svg/sprite_suo24.svg'
+// module.exports = function ({id, size, color}) {
+
+//   retrun (
+//     <svg className="m-svg" fill={color} width={size} height={size} viewBox="0 0 24 24">
+//       <use xlinkHref={`${sprite}#${id}`} />
+//     </svg>
+//   )
+// }
+
+
 import sprite from './svg/sprite_suo24.svg'
 
+import React from "react";
 
 
-module.exports = function ({id, size, color}) {
+export default Svg = ({id, size, color,sprite}) => {
 
-  retrun (
-    <svg className="m-svg" fill={color} width={size} height={size} viewBox="0 0 24 24">
-      <use xlinkHref={`${sprite}#${id}`} />
-    </svg>
+
+  return (
+   <svg className="m-svg" fill={color} width={size} height={size} viewBox="0 0 24 24">
+       <use xlinkHref={`${sprite}#${id}`} />
+   </svg>
   )
+
 }
